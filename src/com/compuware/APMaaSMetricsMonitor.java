b6485c947d4330c2b7c04f568fa48e925f17ebfa
mccon <mccon@kits.com>
2016-06-10 00:06:17 +0100

    Remove unused import

--- src/com/compuware/APMaaSMetricsMonitor.java
+++ src/com/compuware/APMaaSMetricsMonitor.java
@@ -9,7 +9,6 @@ package com.compuware;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
